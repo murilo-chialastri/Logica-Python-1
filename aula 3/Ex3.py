b485caee2ede6afbb50e788b7848421d115da6de
Murilo Castro <murilocastro233@gmail.com>
2026-04-09 16:44:25 -0300

    validador Cpf

--- aula 3/Ex3.py
+++ aula 3/Ex3.py
@@ -1,17 +1,35 @@
-cpf = input("digite cpf")
-#cpf = 11
-digito = cpf[9:]
-print(digito)
-conta1 = (int(cpf[0]) * 10 + int(cpf[1]) * 9 + int(cpf[2]) * 8 + int(cpf[3]) * 7 + int(cpf[4]) * 6
-                                            + int(cpf[5]) * 5 + int(cpf[6]) * 4 + int(cpf[7]) * 3 + int(cpf[8]) * 2  )%11
-if conta1 < 2:
+
+#  PEGAR CPF
+cpf = input("digite cpf: ") #cpf = 11
+digito = cpf[9:] # PEGA 2 ULTIMOS DIGITOS
+#p1 = cpf[9]
+
+#print(digito) #
+if len(cpf) == 11:
+    # PRIMEIRO NUMERO
+    conta1 = (int(cpf[0]) * 10 + int(cpf[1]) * 9 + int(cpf[2]) * 8 + int(cpf[3]) * 7 + int(cpf[4]) * 6
+                                                + int(cpf[5]) * 5 + int(cpf[6]) * 4 + int(cpf[7]) * 3 + int(cpf[8]) * 2  )%11
     digito1 = 0
+    if conta1 < 2:
+        digito1 = 0
+    else:
+        digito1 = 11 - conta1
+    #----------------------------------------------------------------------------------------------------------------------------------
+    #SEGUNDO NUMERO
+    conta2 = (  int(cpf[0]) * 11 + int(cpf[1]) * 10 + int(cpf[2]) * 9 + int(cpf[3]) * 8 + int(cpf[4]) * 7
+                                                + int(cpf[5]) * 6 + int(cpf[6]) * 5 + int(cpf[7]) * 4 + int(cpf[8]) * 3 + digito1 * 2 )%11
+    digito2 = 0
+    if conta2 < 2:
+        digito2 = 0
+    else:
+        digito2 = 11 - conta2
+    #----------------------------------------------------------------------------------------------------------------------------------
+
+    # Válidador de CPF
+    if digito == str(digito1) + str(digito2):
+        print("CPF Válido")
+    else:
+        print("CPF incorreto")
+
 else:
-    digito = 11 - conta1
-
-conta1 = (int(cpf[0]) * 10 + int(cpf[1]) * 9 + int(cpf[2]) * 8 + int(cpf[3]) * 7 + int(cpf[4]) * 6
-                                            + int(cpf[5]) * 5 + int(cpf[6]) * 4 + int(cpf[7]) * 3 + int(cpf[8]) * 2  )%11
-
-print(conta1)
-res = int(digito) == conta
-print(res)
+    print("CPF incorreto!")
