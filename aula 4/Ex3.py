7f7580f4ac11e1a61078537dafdb611ea2b510bf
murilo castro <murilocastro233@gmail.com>
2026-04-15 22:27:56 -0300

    bonus

--- aula 4/Ex3.py
+++ aula 4/Ex3.py
@@ -12,12 +12,19 @@
 #         print(n)
 #     n += 1
 
-num = int(input("digite um numero"))
-n = 1
-p = 0
-while(n <= num):
-    if n % 2 == 0:
-        p = p + n
-    n += 1
-
-print(p)
+# num = int(input("digite um numero"))
+# n = 1
+# p = 0
+# while(n <= num):
+#     if n % 2 == 0:
+#         p = p + n
+#     n += 1
+#
+# print(p)
+soma = 0
+while(True):
+    opc = int(input("Digite um número (0 para parar): "))
+    soma += opc
+    if opc == 0:
+        break
+print(soma)
